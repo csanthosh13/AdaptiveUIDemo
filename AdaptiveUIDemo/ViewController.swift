6b6a906140a570de437ff658994046cd13229f41
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-01-18 22:58:37 +0530

    Initial Commit

---
+++ AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveUIDemo
+//
+//  Created by Santosh Chandrasekharan on 18/01/19.
+//  Copyright © 2019 Santosh Chandrasekharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
